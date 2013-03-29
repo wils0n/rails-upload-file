832f6c8f5e0ba023290cafca73539c510fc96262
wils0n <xitu@Emc2.(none)>
2013-03-29 17:32:56 -0500

    upload

---
+++ admin/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Admin::Application.config.secret_token = '84cc8bfae56e88dc2005a5fa5446b640be477da3e1da07c9cd705d8ae36d9f319ccb9533b406c0bd215690fe537a18da9bc0e971fda257916ad7d8e369046519'
---
+++ admin/config/routes.rb
@@ -0,0 +1,10 @@
+Admin::Application.routes.draw do
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
+  resources :articles
+
+  root :to => 'articles#index'
+
+end
